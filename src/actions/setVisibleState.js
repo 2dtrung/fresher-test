f9d7f83974a721e1b1e50cc071fe7f3fbbde2eba
Duc Trung <sductrung@gmail.com>
2021-09-18 02:11:38 +0700

    add action-store to show nesessary UI

---
+++ src/actions/setVisibleState.js
@@ -0,0 +1,15 @@
+export const setTrue = {
+    type: "SET_TRUE",
+}
+
+export const setFalse = {
+    type: "SET_FALSE",
+}
+
+export const setTrueTQT = {
+    type: "SET_TRUE_TQT",
+}
+
+export const setFalseTQT = {
+    type: "SET_FALSE_TQT",
+}
